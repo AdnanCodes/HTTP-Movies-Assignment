3718b13cf273d7b8a0096ecbe7b34390b49bdd6a
Adnan <adnan.chowdhury@outlook.com>
2019-08-22 15:32:45 -0500

    built form for editing

---
+++ client/src/Movies/UpdateMovieForm.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const initialMovie = {
+  id: "",
+  title: "",
+  director: "",
+  metascore: "",
+  stars: []
+};
+
+const UpdateForm = () => {
+  const [movie, setMovie] = useState(initialMovie);
+
+  const handlechange = e => {
+    setMovie({
+      ...movie,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(movie);
+  };
+
+  return (
+    <div>
+      <h2>Update Movie Form</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          name="id"
+          placeholder="Enter the ID"
+          value={movie.id}
+          onChange={handlechange}
+        />
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter the Title"
+          value={movie.title}
+          onChange={handlechange}
+        />
+        <input
+          type="text"
+          name="director"
+          placeholder="Enter the Director"
+          value={movie.director}
+          onChange={handlechange}
+        />
+        <input
+          type="text"
+          name="stars"
+          placeholder="Enter the Cast Stars"
+          value={movie.stars}
+          onChange={handlechange}
+        />
+        <button>Edit the Movie</button>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateForm;
